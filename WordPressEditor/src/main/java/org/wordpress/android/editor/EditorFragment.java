c3ea8384c44facd176ed29fccaa24c325a552e7d
Alex Forcier <alex.forcier@automattic.com>
2015-05-06 09:40:46 -0400

    Added an EditorFragmentForTests class, overriding certain EditorFragment methods for more testing support

--- WordPressEditor/src/main/java/org/wordpress/android/editor/EditorFragment.java
+++ WordPressEditor/src/main/java/org/wordpress/android/editor/EditorFragment.java
@@ -118,7 +118,7 @@ public class EditorFragment extends EditorFragmentAbstract implements View.OnCli
         super.onDetach();
     }
 
-    private void initJsEditor() {
+    protected void initJsEditor() {
         String htmlEditor = Utils.getHtmlFromFile(mActivity, "android-editor.html");
 
         mWebView.addJavascriptInterface(new JsCallbackReceiver(this), JS_CALLBACK_HANDLER);
