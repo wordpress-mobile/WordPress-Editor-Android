d2bf4966abc3e10084a6b6c383cd7ed9beb5f1ee
Alex Forcier <alex.forcier@automattic.com>
2015-05-22 10:54:01 -0400

    Changed Utils.buildMapFromKeyValuePairs() to only treat the first = as a delimiter
    - This allows the method to handle input with multiple '=', like `contents=<a href="...">'
    - Added some more Utils unit tests

--- WordPressEditor/src/main/java/org/wordpress/android/editor/Utils.java
+++ WordPressEditor/src/main/java/org/wordpress/android/editor/Utils.java
@@ -76,9 +76,9 @@ public class Utils {
     public static Map<String, String> buildMapFromKeyValuePairs(Set<String> keyValueSet) {
         Map<String, String> selectionArgs = new HashMap<>();
         for (String pair : keyValueSet) {
-            String[] splitString = pair.split("=");
-            if (splitString.length == 2) {
-                selectionArgs.put(splitString[0], splitString[1]);
+            int delimLoc = pair.indexOf("=");
+            if (delimLoc != -1) {
+                selectionArgs.put(pair.substring(0, delimLoc), pair.substring(delimLoc + 1));
             }
         }
         return selectionArgs;
