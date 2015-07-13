947c553bf173f1064c3304e58f374a49c8e9a692
Alex Forcier <alex.forcier@automattic.com>
2015-07-13 16:44:23 -0400

    Added more HtmlStyleTextWatcher tests

--- WordPressEditor/src/test/java/org/wordpress/android/editor/HtmlStyleTextWatcherTest.java
+++ WordPressEditor/src/test/java/org/wordpress/android/editor/HtmlStyleTextWatcherTest.java
@@ -2,6 +2,9 @@ package org.wordpress.android.editor;
 
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -256,8 +259,74 @@ public class HtmlStyleTextWatcherTest {
             mCountDownLatch.await();
             assertEquals(offset, mSpanRange.getOpeningTagLoc());
             assertEquals(offset + 4, mSpanRange.getClosingTagLoc());
-
         }
+    }
+
+    @Test
+    public void testTypingInTagWithSurroundingTags() throws InterruptedException {
+        // Spans in this case will be applied until the end of the next tag
+        // This fixes a pasting bug and might be refined later
+        // -- Test typing in the opening tag symbol
+        mCountDownLatch = new CountDownLatch(1);
+        mContent = new SpannableStringBuilder("some <del>text</del> < <b>bold text</b>");
+
+        mWatcher.onTextChanged(mContent, 21, 0, 1); // Added lone "<"
+        mWatcher.afterTextChanged(mContent);
+
+        mCountDownLatch.await();
+        assertEquals(21, mSpanRange.getOpeningTagLoc());
+        assertEquals(26, mSpanRange.getClosingTagLoc());
+
+
+        // -- Test typing in the tag name
+        mCountDownLatch = new CountDownLatch(1);
+        mContent = new SpannableStringBuilder("some <del>text</del> <i <b>bold text</b>");
+
+        mWatcher.onTextChanged(mContent, 22, 0, 1);
+        mWatcher.afterTextChanged(mContent);
+
+        mCountDownLatch.await();
+        assertEquals(21, mSpanRange.getOpeningTagLoc());
+        assertEquals(27, mSpanRange.getClosingTagLoc());
+
+
+        // -- Test typing in the closing tag symbol
+        mCountDownLatch = new CountDownLatch(1);
+        mContent = new SpannableStringBuilder("some <del>text</del> <i> <b>bold text</b>");
+
+        mWatcher.onTextChanged(mContent, 23, 0, 1);
+        mWatcher.afterTextChanged(mContent);
+
+        mCountDownLatch.await();
+        assertEquals(21, mSpanRange.getOpeningTagLoc());
+        assertEquals(28, mSpanRange.getClosingTagLoc());
+    }
+
+    @Test
+    public void testTypingInLoneClosingSymbol() throws InterruptedException {
+        // -- Test typing in an isolated closing tag symbol
+        mCountDownLatch = new CountDownLatch(1);
+        mContent = new SpannableStringBuilder("some text >");
+
+        mWatcher.onTextChanged(mContent, 10, 0, 1);
+        mWatcher.afterTextChanged(mContent);
+
+        // No formatting should be applied/removed
+        boolean updateSpansWasCalled = mCountDownLatch.await(500, TimeUnit.MILLISECONDS);
+        assertEquals(false, updateSpansWasCalled);
+
+
+        // -- Test typing in an isolated closing tag symbol with surrounding tags
+        mCountDownLatch = new CountDownLatch(1);
+        mContent = new SpannableStringBuilder("some <b>tex>t</b>");
+
+        mWatcher.onTextChanged(mContent, 11, 0, 1); // Added lone ">"
+        mWatcher.afterTextChanged(mContent);
+
+        // The span in this case will be applied from the start of the previous tag to the end of the next tag
+        mCountDownLatch.await();
+        assertEquals(5, mSpanRange.getOpeningTagLoc());
+        assertEquals(17, mSpanRange.getClosingTagLoc());
     }
 
     @Test
@@ -303,6 +372,45 @@ public class HtmlStyleTextWatcherTest {
             assertEquals(offset, mSpanRange.getOpeningTagLoc());
             assertEquals(offset + 5, mSpanRange.getClosingTagLoc());
         }
+    }
+
+    @Test
+    public void testNoChange() throws InterruptedException {
+        mCountDownLatch = new CountDownLatch(1);
+
+        mWatcher.beforeTextChanged("sample", 0, 0, 0);
+        mWatcher.onTextChanged("sample", 0, 0, 0);
+        mWatcher.afterTextChanged(null);
+
+        // No formatting should be applied/removed
+        boolean updateSpansWasCalled = mCountDownLatch.await(500, TimeUnit.MILLISECONDS);
+        assertEquals(false, updateSpansWasCalled);
+    }
+
+    @Test
+    public void testUpdateSpans() {
+        // -- Test tag styling
+        HtmlStyleTextWatcher watcher = new HtmlStyleTextWatcher();
+        SpannableStringBuilder content = new SpannableStringBuilder("<b>stuff</b>");
+        watcher.updateSpans(content, new HtmlStyleTextWatcher.SpanRange(0, 3));
+
+        assertEquals(1, content.getSpans(0, 3, ForegroundColorSpan.class).length);
+
+        // -- Test entity styling
+        content = new SpannableStringBuilder("text &amp; more text");
+        watcher.updateSpans(content, new HtmlStyleTextWatcher.SpanRange(5, 10));
+
+        assertEquals(1, content.getSpans(5, 10, ForegroundColorSpan.class).length);
+        assertEquals(1, content.getSpans(5, 10, StyleSpan.class).length);
+        assertEquals(1, content.getSpans(5, 10, RelativeSizeSpan.class).length);
+
+        // -- Test comment styling
+        content = new SpannableStringBuilder("text <!--comment--> more text");
+        watcher.updateSpans(content, new HtmlStyleTextWatcher.SpanRange(5, 19));
+
+        assertEquals(1, content.getSpans(5, 19, ForegroundColorSpan.class).length);
+        assertEquals(1, content.getSpans(5, 19, StyleSpan.class).length);
+        assertEquals(1, content.getSpans(5, 19, RelativeSizeSpan.class).length);
     }
 
     private class HtmlStyleTextWatcherForTests extends HtmlStyleTextWatcher {
