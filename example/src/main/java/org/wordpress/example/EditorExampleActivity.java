021b485bd4c1b7a846bfb44a7845d7b7f02a189d
Alex Forcier <alex.forcier@automattic.com>
2015-11-24 08:02:10 -0500

    Fixed onMediaUploadSucceeded implementation in the example activity

--- example/src/main/java/org/wordpress/example/EditorExampleActivity.java
+++ example/src/main/java/org/wordpress/example/EditorExampleActivity.java
@@ -31,7 +31,7 @@ public class EditorExampleActivity extends AppCompatActivity implements EditorFr
     public static final int ADD_MEDIA_ACTIVITY_REQUEST_CODE = 1111;
     public static final int ADD_MEDIA_FAIL_ACTIVITY_REQUEST_CODE = 1112;
 
-    public static final int MEDIA_REMOTE_ID_SAMPLE = 123;
+    public static final String MEDIA_REMOTE_ID_SAMPLE = "123";
 
     private static final int SELECT_PHOTO_MENU_POSITION = 0;
     private static final int SELECT_PHOTO_FAIL_MENU_POSITION = 1;
@@ -182,13 +182,13 @@ public class EditorExampleActivity extends AppCompatActivity implements EditorFr
                     while (count < 1.1) {
                         sleep(500);
 
-                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId,
-                                MEDIA_REMOTE_ID_SAMPLE, count);
+                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId, count);
 
                         count += 0.1;
                     }
 
-                    ((EditorMediaUploadListener) mEditorFragment).onMediaUploadSucceeded(mediaId, mediaUrl);
+                    ((EditorMediaUploadListener) mEditorFragment).onMediaUploadSucceeded(mediaId,
+                            MEDIA_REMOTE_ID_SAMPLE, mediaUrl);
 
                     if (mFailedUploads.containsKey(mediaId)) {
                         mFailedUploads.remove(mediaId);
@@ -211,8 +211,7 @@ public class EditorExampleActivity extends AppCompatActivity implements EditorFr
                     while (count < 0.6) {
                         sleep(500);
 
-                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId,
-                                MEDIA_REMOTE_ID_SAMPLE, count);
+                        ((EditorMediaUploadListener) mEditorFragment).onMediaUploadProgress(mediaId, count);
 
                         count += 0.1;
                     }
