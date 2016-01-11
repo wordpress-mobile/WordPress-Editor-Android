e9e8f7f063140ab927772af39af44c6d74e80408
Alex Forcier <alex.forcier@automattic.com>
2016-01-11 11:20:38 -0500

    Implemented missing method in example app

--- example/src/main/java/org/wordpress/example/EditorExampleActivity.java
+++ example/src/main/java/org/wordpress/example/EditorExampleActivity.java
@@ -163,6 +163,11 @@ public class EditorExampleActivity extends AppCompatActivity implements EditorFr
     }
 
     @Override
+    public String onAuthHeaderRequested(String url) {
+        return "";
+    }
+
+    @Override
     public void onEditorFragmentInitialized() {
         // arbitrary setup
         mEditorFragment.setFeaturedImageSupported(true);
